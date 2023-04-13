75c68540392149dbc5be5a7fe0b79f425b2ca000
Jimb Esser <wasteland@gmail.com>
2023-04-13 06:02:35 -0700

    Add script to push to web repo

---
+++ push_web.bat
@@ -0,0 +1,11 @@
+@SET TORTOISE_PATH=%ProgramW6432%\TortoiseSVN\bin\TortoiseProc.exe
+
+call node build build
+
+rd /s /q ..\..\SRC2\web\dashingstrike.com\LudumDare\DCJ2023
+md ..\..\SRC2\web\dashingstrike.com\LudumDare\DCJ2023
+xcopy /s dist\game\build.prod\client\ ..\..\SRC2\web\dashingstrike.com\LudumDare\DCJ2023\
+@for /F usebackq %%a in (`git rev-parse HEAD`) do SET VER=%%a
+"%TORTOISE_PATH%" /command:commit /path:..\..\SRC2\web\dashingstrike.com\LudumDare\DCJ2023  /logmsg:"Jam update from git %VER%"
+
+@echo Now, deploy flightplan web-prd.
